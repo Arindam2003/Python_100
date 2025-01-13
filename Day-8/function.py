ddba4347180bac566e5a15ba2038fb02e7ba313f
Arindam2003 <dindaarindam000@gmail.com>
2025-01-13 23:21:46 +0530

    project-8 half complete

---
+++ Day-8/function.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("Arindam")
+#     print("Dinda")
+#     print("Sahbe")
+
+# greet()
+
+def greet_with(name,location):
+    print(name)
+    print(f"From {location}.")
+
+greet_with("Arindam","Panskura")
