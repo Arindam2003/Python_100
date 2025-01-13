95c175122a8f5272d2068020db1e61d4527eeed4
Arindam2003 <dindaarindam000@gmail.com>
2025-01-13 23:53:33 +0530

    My Love Calculator

--- Day-8/love_calculator.py
+++ Day-8/love_calculator.py
@@ -3,17 +3,29 @@ def calculate_love_score(name1,name2):
     print(combined)
     lower=combined.lower()
     print(lower)
-    count1=0
+    first=0
     x="true"
     for j in x:
-        for i in name1:
-            count1=count1+name1.count(i)
-    print(count1)
-    name2.lower()
+        for i in combined:
+            if(j is i):
+                first=first+1
+    print(first) 
+
+    second=0
+    x="love"
+    for j in x:
+        for i in combined:
+            if(j is i):
+                second=second+1
+    print(second)
+
+    # print(count1)
+    # name2.lower()
     # count2=0
     # for i in name2:
     #     count2=count2+name2.count(i)
     # print(count2)
-    # print(str(count1)+str(count2))
+    print("Love Percentage is: ")
+    print(str(first)+str(second))
     
 calculate_love_score("Arindam", "Dinda")
